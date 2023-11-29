2f5ebf2b85de0a77a46c6875eec6fa9c51b1a34d
Oleksandr <sa44ko@gmail.com>
2023-11-29 13:07:29 +0200

    Split the code into methods.

--- app/src/main/java/sd/java/lab2/Lab2Application.java
+++ app/src/main/java/sd/java/lab2/Lab2Application.java
@@ -8,10 +8,25 @@ public class Lab2Application {
     Lab2Application(){}
 
     public static void run() {
-        long[][] matrixA;
-        long[][] matrixB;
-        long[][] resultMatrix;
+        int[] sidesSize = getMatrixSides();
+        final int width = sidesSize[0];
+        final int height = sidesSize[1];
 
+        long[][] matrixA = generateRandomMatrix(height, width);
+        long[][] matrixB = generateRandomMatrix(height, width);
+        long[][] resultMatrix = addMatrices(matrixA, matrixB);
+
+        printOutMatrix("Matrix A", matrixA);
+
+        printOutMatrix("Matrix B", matrixB);
+
+        printOutMatrix("Result matrix", resultMatrix);
+
+        System.out.println("\nSum of the lowest in the row of matrix: " + sumOfTheLowestInTheRow(resultMatrix));
+
+    }
+
+    private static int[] getMatrixSides() {
         Scanner scanner = new Scanner(System.in);
         final String[] sides = new String[]{"width", "height"};
         int[] sidesSize = new int[2];
@@ -33,38 +48,39 @@ public class Lab2Application {
                 scanner.next();
             }
         }
+        return sidesSize;
+    }
 
-        final int width = sidesSize[0];
-        final int height = sidesSize[1];
-
+    private static long[][] generateRandomMatrix(int height, int width) {
         Random random = new Random();
-        matrixA = new long[height][width];
-        matrixB = new long[height][width];
-        resultMatrix = new long[height][width];
-
+        long[][] matrix = new long[height][width];
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                matrixA[i][j] = random.nextLong(-100,100);
-                matrixB[i][j] = random.nextLong(-100,100);
+                matrix[i][j] = random.nextLong(-100,100);
+            }
+        }
+
+        return matrix;
+    }
+
+    private static long[][] addMatrices(long[][] matrixA, long[][] matrixB) {
+        int height = matrixA.length;
+        int width = matrixA[0].length;
+
+        long[][] resultMatrix = new long[height][width];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++){
                 resultMatrix[i][j] = matrixA[i][j] + matrixB[i][j];
             }
         }
 
-        System.out.println("\nMatrix A: \n");
-        printOutMatrix(matrixA);
-
-        System.out.println("\nMatrix B: \n");
-        printOutMatrix(matrixB);
-
-        System.out.println("\nResult matrix: \n");
-        printOutMatrix(resultMatrix);
-
-        System.out.println("\nSum of the lowest in the row of matrix: " + sumOfTheLowestInTheRow(resultMatrix));
-
+        return resultMatrix;
     }
 
-    private static void printOutMatrix(long[][] matrix) {
+    private static void printOutMatrix(String name, long[][] matrix) {
+        System.out.println("\n" + name + ": \n");
         for (long[] longs : matrix) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (j == matrix[0].length - 1) {
